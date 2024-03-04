6a4e26673169ca85c2e6cf9fb0db4e5d2c6f11a3
kenmoli <46527408+kenmoli@users.noreply.github.com>
2024-03-04 14:05:00 +0800

    Imported stitch route to server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 
-from flask_routes import status, image, path
+from flask_routes import status, image, path, stitch
 
 # Initialisation
 app = Flask(__name__)
@@ -9,8 +9,9 @@ CORS(app)
 
 # Flask Routes
 app.register_blueprint(status, url_prefix="/")
-app.register_blueprint(image, url_prefix="/")
 app.register_blueprint(path, url_prefix="/")
+app.register_blueprint(image, url_prefix="/")
+app.register_blueprint(stitch, url_prefix="/")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
