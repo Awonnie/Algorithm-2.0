a292d7df4e5a61dfb8f12e1bf48f6590d0cd4623
Awonnie <97012983+Awonnie@users.noreply.github.com>
2024-03-05 15:17:22 +0800

    Update obstacle.py

--- arena_objects/obstacle.py
+++ arena_objects/obstacle.py
@@ -1,9 +1,11 @@
 from typing import List
 
+from consts import SCREENSHOT_COST, VIRTUAL_CELLS
 from direction import Direction
+
 from .grid_cell import GridCell
 from .helper import is_valid_position
-from consts import VIRTUAL_CELLS, SCREENSHOT_COST
+
 
 class Obstacle(GridCell):
     """Obstacle class, inherited from GridCell"""
@@ -36,34 +38,34 @@ class Obstacle(GridCell):
         center_viewing_coord = (0,0)
         robot_direction = Direction.NONE
 
-        match self.direction:
-            # If the image is facing NORTH, robot has to be facing SOUTH
-            case Direction.NORTH:
-                center_viewing_coord = (self.x, self.y + extra_cells)
-                left_viewing_coord = (self.x + 1, self.y + extra_cells)
-                right_viewing_coord = (self.x - 1, self.y + extra_cells)
-                robot_direction = Direction.SOUTH
 
-            # If the image is facing SOUTH, robot has to be facing NORTH
-            case Direction.SOUTH:
-                center_viewing_coord = (self.x, self.y - extra_cells)
-                left_viewing_coord = (self.x - 1, self.y - extra_cells)
-                right_viewing_coord = (self.x + 1, self.y - extra_cells)
-                robot_direction = Direction.NORTH
+        # If the image is facing NORTH, robot has to be facing SOUTH
+        if self.direction==Direction.NORTH:
+            center_viewing_coord = (self.x, self.y + extra_cells)
+            left_viewing_coord = (self.x + 1, self.y + extra_cells)
+            right_viewing_coord = (self.x - 1, self.y + extra_cells)
+            robot_direction = Direction.SOUTH
 
-            # If the image is facing WEST, robot has to be facing EAST
-            case Direction.WEST:
-                center_viewing_coord = (self.x - extra_cells, self.y)
-                left_viewing_coord = (self.x - extra_cells, self.y + 1)
-                right_viewing_coord = (self.x - extra_cells, self.y - 1)
-                robot_direction = Direction.EAST
+        # If the image is facing SOUTH, robot has to be facing NORTH
+        elif self.direction==Direction.SOUTH:
+            center_viewing_coord = (self.x, self.y - extra_cells)
+            left_viewing_coord = (self.x - 1, self.y - extra_cells)
+            right_viewing_coord = (self.x + 1, self.y - extra_cells)
+            robot_direction = Direction.NORTH
 
-            # If the image is facing EAST, robot has to be facing WEST
-            case Direction.EAST:
-                center_viewing_coord = (self.x + extra_cells, self.y)
-                left_viewing_coord = (self.x + extra_cells, self.y - 1)
-                right_viewing_coord = (self.x + extra_cells, self.y + 1)
-                robot_direction = Direction.WEST
+        # If the image is facing WEST, robot has to be facing EAST
+        elif self.direction==Direction.WEST:
+            center_viewing_coord = (self.x - extra_cells, self.y)
+            left_viewing_coord = (self.x - extra_cells, self.y + 1)
+            right_viewing_coord = (self.x - extra_cells, self.y - 1)
+            robot_direction = Direction.EAST
+
+        # If the image is facing EAST, robot has to be facing WEST
+        elif self.direction== Direction.EAST:
+            center_viewing_coord = (self.x + extra_cells, self.y)
+            left_viewing_coord = (self.x + extra_cells, self.y - 1)
+            right_viewing_coord = (self.x + extra_cells, self.y + 1)
+            robot_direction = Direction.WEST
             
         # Center Position 
         if is_valid_position(center_viewing_coord[0], center_viewing_coord[1]):
